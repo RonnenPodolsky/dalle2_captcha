94c1fea3cd9ccc7d3fe035f6bdbd8d60836dce46
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-12-15 15:14:33 +0200

    use call back

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -8,6 +8,7 @@ function App() {
   const [options, setOptions] = useState(null);
   const [valid, setValid] = useState(null);
   const [status, setStatus] = useState(false);
+  const isCancelled = useRef(false);
 
   const checkIfRobot = (e) => {
     if (e.target.classList[0] === valid) {
@@ -18,7 +19,7 @@ function App() {
     }
   }
 
-  const getData = async () => {
+  const getData = useCallback(async () => {
     const dataJson = await fetch("/api");
     const dataObj = await dataJson.json();
     console.log(dataObj)
@@ -26,20 +27,19 @@ function App() {
     setOptions(dataObj.options)
     setImage(dataObj.image)
     setLoading(false)
-  }
+  }, [])
 
   useEffect(() => {
-    let isCancelled = true;
-    if (isCancelled) {
+    console.log('mounted')
+    if (isCancelled.current) {
       getData();
     }
     return () => {
-      console.log('s')
-      isCancelled = false;
+      console.log('unmounted')
+      isCancelled.current = true;
     }
-  }, []);
+  }, [getData]);
 
-  if (loading) return <h2>loading...</h2>
   
   return (
     <div className="App">
@@ -47,14 +47,18 @@ function App() {
 
         <h1>Are you a Robot?</h1>
         <h2>What is the following image?</h2>
-        {image && <img src={image} alt={options[valid]} />}
-        <div className='options'>
-          {options?.map((option) => {
-            return <div onClick={checkIfRobot} className={option} key={option}>{option}</div>
-          })}
+        {loading ? <h2>loading...</h2> : 
+        <div>
+                  {<img src={image} alt={options[valid]} />}
+                  <div className='options'>
+                    {options?.map((option) => {
+                      return <div onClick={checkIfRobot} className={option} key={option}>{option}</div>
+                    })}
+          
+                  </div> 
+                  {status ? <h3> not a robot!</h3> : <h3>robot?</h3>}
+        </div>}
 
-        </div>
-        {status ? <h3> not a robot!</h3> : <h3>robot?</h3>}
       </header>
     </div>
   )
